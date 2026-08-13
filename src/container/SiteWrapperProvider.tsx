6aa8645ba4c5acc32f92ae0bfd9ce8db4c9cf133
Montel Anthony <58714976+Montelent@users.noreply.github.com>
2026-08-13 19:10:42 +0100

    Refactor SiteWrapperProvider component
    Refactor SiteWrapperProvider to remove unused components and make __TEMPLATE_QUERY_DATA__ optional.

--- src/container/SiteWrapperProvider.tsx
+++ src/container/SiteWrapperProvider.tsx
@@ -1,25 +1,16 @@
 import React, { FC } from 'react'
 import { Provider } from 'react-redux'
 import { store } from '@/stores/store'
-import { SiteWrapperChild } from './SiteWrapperChild'
-import LoginModal from './LoginModal'
 
 interface SiteWrapperProviderProps {
 	children: React.ReactNode
-	__TEMPLATE_QUERY_DATA__: any
+	__TEMPLATE_QUERY_DATA__?: any
 }
 
 const SiteWrapperProvider: FC<SiteWrapperProviderProps> = ({
 	children,
-	...props
 }) => {
-	return (
-		<Provider store={store}>
-			{children}
-			<SiteWrapperChild {...props} />
-			<LoginModal />
-		</Provider>
-	)
+	return <Provider store={store}>{children}</Provider>
 }
 
 export default SiteWrapperProvider
