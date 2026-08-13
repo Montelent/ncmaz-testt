75f99cd1ca590b5a11f46ac8afa5e784eba29c90
Montel Anthony <58714976+Montelent@users.noreply.github.com>
2026-08-14 00:10:30 +0100

    Use environment variable for WordPress GraphQL URL
    Refactor code to use wordpressUrl for GraphQL schema.

--- codegen.ts
+++ codegen.ts
@@ -2,22 +2,26 @@ import { CodegenConfig } from '@graphql-codegen/cli'
 const { loadEnvConfig } = require('@next/env')
 loadEnvConfig(process.cwd())
 
+const wordpressUrl = process.env.NEXT_PUBLIC_WORDPRESS_URL?.replace(/\/$/, '')
+
+if (!wordpressUrl) {
+  console.error('❌ NEXT_PUBLIC_WORDPRESS_URL is not set. Codegen cannot fetch the GraphQL schema.')
+  process.exit(1)
+}
+
 const config: CodegenConfig = {
-	schema: `${process.env.NEXT_PUBLIC_WORDPRESS_URL?.replace(
-		/\/$/,
-		'',
-	)}/graphql`,
-	documents: ['src/**/*.{tsx,ts}'],
-	generates: {
-		'./src/__generated__/': {
-			preset: 'client',
-			plugins: [],
-			presetConfig: {
-				gqlTagName: 'gql',
-			},
-		},
-	},
-	ignoreNoDocuments: true,
+  schema: `${wordpressUrl}/graphql`,
+  documents: ['src/**/*.{tsx,ts}'],
+  generates: {
+    './src/__generated__/': {
+      preset: 'client',
+      plugins: [],
+      presetConfig: {
+        gqlTagName: 'gql',
+      },
+    },
+  },
+  ignoreNoDocuments: true,
 }
 
 export default config
