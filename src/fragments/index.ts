52d6af27919d210398ee3f78652199c9159e9469
Montel Anthony <58714976+Montelent@users.noreply.github.com>
2026-08-16 10:40:32 +0100

    Add CoreImageFragment and CoreGalleryFragment to index

--- src/fragments/index.ts
+++ src/fragments/index.ts
@@ -229,6 +229,8 @@ export const NC_POST_FULL_VS_EDITOR_BLOCK_NO_CONTENT_FRAGMENT = gql(
 				...NcmazFaustBlockGroupFragment
 				...CoreColumnsFragment
 				...CoreColumnFragment
+				...CoreImageFragment
+				...CoreGalleryFragment
 			}
 			author {
 				node {
