ee15a917a139535681660147501a0685813c87ec
Montel Anthony <58714976+Montelent@users.noreply.github.com>
2026-08-16 12:04:29 +0100

    Fix className and quality prop in MyImage component

--- src/components/MyImage.tsx
+++ src/components/MyImage.tsx
@@ -9,15 +9,27 @@ export interface Props extends ImageProps {
 const MyImage: FC<Props> = ({
 	enableDefaultPlaceholder = false,
 	defaultPlaceholderDataUrl = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8+vx1PQAIqAM4jZDFJQAAAABJRU5ErkJggg==',
+	quality = 70,
 	...props
 }) => {
 	return (
 		<Image
 			{...props}
-			className={`${props.className} ${
+			quality={quality}
+			className={`${props.className || ''} ${
 				props.src ? '' : 'dark:brightness-75 dark:filter'
 			}`}
 			src={props.src || '/images/placeholder.png'}
+			placeholder={
+				enableDefaultPlaceholder
+					? 'blur'
+					: props.placeholder
+			}
+			blurDataURL={
+				enableDefaultPlaceholder
+					? defaultPlaceholderDataUrl
+					: props.blurDataURL
+			}
 		/>
 	)
 }
