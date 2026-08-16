41fe19654c49d2bdc4bdafc470e275c225b58013
Montel Anthony <58714976+Montelent@users.noreply.github.com>
2026-08-16 12:03:05 +0100

    Update next.config.js with compression and headers

--- next.config.js
+++ next.config.js
@@ -8,7 +8,12 @@ module.exports = withFaust({
 	trailingSlash: true,
 	reactStrictMode: true,
 	typedRoutes: false,
+	compress: true,
+	poweredByHeader: false,
 	images: {
+		formats: ['image/avif', 'image/webp'],
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
 		remotePatterns: [
 			{
 				protocol: 'http',
@@ -70,7 +75,6 @@ module.exports = withFaust({
 				port: '',
 				pathname: '/**',
 			},
-			// from env
 			{
 				protocol: 'https',
 				hostname:
@@ -98,6 +102,33 @@ module.exports = withFaust({
 						{ uri: process.env.NEXT_PUBLIC_WORDPRESS_URL },
 					],
 				}),
+			},
+			{
+				source: '/_next/static/:path*',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 'public, max-age=31536000, immutable',
+					},
+				],
+			},
+			{
+				source: '/favicons/:path*',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 'public, max-age=86400, stale-while-revalidate=604800',
+					},
+				],
+			},
+			{
+				source: '/images/:path*',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 'public, max-age=86400, stale-while-revalidate=604800',
+					},
+				],
 			},
 		]
 	},
