2a4bb60fabd16757e54a2450289b0564e5f6fdd8
Montel Anthony <58714976+Montelent@users.noreply.github.com>
2026-08-23 02:38:47 +0100

    a11y: aria-label on logo home link

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -17,22 +17,26 @@ const Logo: React.FC<LogoProps> = ({ className = '', imageClassName }) => {
 		return null
 	}
 
+	const siteName =
+		NC_SITE_SETTINGS.site_info?.site_title || 'SammyGuru home'
+
 	return (
 		<Link
 			href="/"
+			aria-label={siteName}
 			className={`ttnc-logo inline-block flex-shrink-0 text-primary-600 ${className}`}
 		>
 			<MyImage
 				className={'block w-12 sm:w-14 dark:hidden ' + imageClassName}
 				src={logoSrc || ''}
-				alt={'Logo'}
+				alt={siteName}
 				width={56}
 				height={56}
 			/>
 			<MyImage
 				className={'hidden w-12 sm:w-14 dark:block ' + imageClassName}
 				src={logoLightSrc || ''}
-				alt={'Logo'}
+				alt=""
 				width={56}
 				height={56}
 			/>
